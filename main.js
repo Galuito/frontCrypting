009914652269ebff2f409a256a25b3c9e079b891
Galuito <nluisgalue20@hotmail.com>
2023-10-01 01:17:32 -0400

    Handled the file sending logic and success screen

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const express = require("express");
 const https = require("https");
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const port = 5120;
+app.use(cors());
 
 app.get('/', (req, res) =>{
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'html', "index.html"));
+})
+
+app.get('/success', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'html', "success.html"));
 })
 
 const sslServer = https.createServer({
@@ -15,6 +21,7 @@ const sslServer = https.createServer({
   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
 }, app)
 
+// To access https://127.0.0.1:5120
 sslServer.listen(port, ()=>{
   console.log("SSL Frontend listening on port", port)
 })
