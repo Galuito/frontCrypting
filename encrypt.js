36e7b1c80fc25227416a04dfb9224477e6b31f19
Galuito <nluisgalue20@hotmail.com>
2023-10-01 01:26:49 -0400

    Started the encrypt.js file

---
+++ encrypt.js
@@ -0,0 +1,4 @@
+/**
+ * Encrypting module in charge of doing both symmetric and asymmetric encryption, logically it is placed here
+ * because it should be a part of the client in case we were to implement the encryption directly here.
+ */
